c42893700a995a0b4a618926544c0b4e75dd7c0b
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2021-11-24 01:57:10 +0200

    Run gdformat and gdlint
    Also fixed issue with the main scene not being loaded

--- gdgifexporter/quantization/uniform.gd
+++ gdgifexporter/quantization/uniform.gd
@@ -1,11 +1,11 @@
 extends Reference
 
-
-var converter = preload('../converter.gd').new()
+var converter = preload("../converter.gd").new()
 var transparency := false
 
+
 func how_many_divisions(colors_count: int) -> int:
-	return int(ceil( pow(colors_count, 1.0 / 4.0) ))
+	return int(ceil(pow(colors_count, 1.0 / 4.0)))
 
 
 func generate_colors(colors_count: int) -> Array:
@@ -16,26 +16,39 @@ func generate_colors(colors_count: int) -> Array:
 		for b in range(divisions_count):
 			for g in range(divisions_count):
 				for r in range(divisions_count):
-					colors.append([Vector3(
-							(255.0 / divisions_count) * r,
-							(255.0 / divisions_count) * g,
-							(255.0 / divisions_count) * b),
-							(255.0 / divisions_count) * a])
+					colors.append(
+						[
+							Vector3(
+								(255.0 / divisions_count) * r,
+								(255.0 / divisions_count) * g,
+								(255.0 / divisions_count) * b
+							),
+							(255.0 / divisions_count) * a
+						]
+					)
 
 	return colors
+
 
 func find_nearest_color(palette_color: Vector3, image_data: PoolByteArray) -> Array:
 	var nearest_color = null
 	var nearest_alpha = null
 	for i in range(0, image_data.size(), 4):
-			var color = Vector3(image_data[i], image_data[i + 1], image_data[i + 2])
-			# detect transparency
-			if image_data[3] == 0:
-				transparency = true
-			if (nearest_color == null) or (palette_color.distance_squared_to(color) < palette_color.distance_squared_to(nearest_color)):
-				nearest_color = color
-				nearest_alpha = image_data[i + 3]
+		var color = Vector3(image_data[i], image_data[i + 1], image_data[i + 2])
+		# detect transparency
+		if image_data[3] == 0:
+			transparency = true
+		if (
+			(nearest_color == null)
+			or (
+				palette_color.distance_squared_to(color)
+				< palette_color.distance_squared_to(nearest_color)
+			)
+		):
+			nearest_color = color
+			nearest_alpha = image_data[i + 3]
 	return [nearest_color, nearest_alpha]
+
 
 # moves every color from palette colors to the nearest found color in image
 func enhance_colors(image: Image, palette_colors: Array) -> Array:
@@ -47,11 +60,13 @@ func enhance_colors(image: Image, palette_colors: Array) -> Array:
 
 	return palette_colors
 
+
 func to_color_array(colors: Array) -> Array:
 	var result := []
 	for v in colors:
 		result.append([v[0].x, v[0].y, v[0].z])
 	return result
+
 
 # quantizes to gif ready codes
 func quantize(image: Image) -> Array:
@@ -67,7 +82,7 @@ func quantize(image: Image) -> Array:
 
 	image.unlock()
 	colors = to_color_array(colors)
-	
+
 	var data: PoolByteArray = converter.get_similar_indexed_datas(image, colors)
-	
+
 	return [data, colors, transparency]
